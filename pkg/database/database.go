ad0a66620dbd5e07374461fe03034fcf1d0ff708
saman <saman.hoseini@snapp.cab>
2021-04-01 20:03:32 +0430

    feat: add database

---
+++ pkg/database/database.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"errors"
+	"github.com/saman2000hoseini/virtual-box_management_server/internal/app/virtual-box/model"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+const (
+	healthCheckInterval = 1
+	maxAttempts         = 60
+)
+
+func NewDB() (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open("./myDB.db"), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+//create tables from models
+func migrate(db *gorm.DB) error {
+	err := db.AutoMigrate(&model.User{})
+	return err
+}
+
+//make database and create tables for the first time
+func FirstSetup() (*gorm.DB, error) {
+	db, err := NewDB()
+	if err != nil {
+		return nil, errors.New("error on creating db")
+	}
+	err = migrate(db)
+	if err != nil {
+		return nil, errors.New("error on creating tables" + err.Error())
+	}
+	return db, nil
+}
